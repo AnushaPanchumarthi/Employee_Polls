a3c8e1e6e7904f24f6c783f9ee46fe64f5f9aaf3
AnushaPanchumarthi <anooshap.rao@gmail.com>
2022-07-27 12:44:51 +0530

    Create Dashboard.js
    Initial commit

---
+++ employee_polls_wyr/src/components/Dashboard.js
@@ -0,0 +1,45 @@
+import { connect } from 'react-redux';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import QuestionsLayout from './PageLayout/QuestionsLayout';
+
+const Dashboard = (props) => {
+  const navigate = useNavigate();
+  if (props.loaded === undefined) {
+    return <p className="text-center mt-4">Loading...</p>;
+  } else if (props.authedUser === null) {
+    navigate('/login');
+  } else {
+    const { questionIds } = props;
+    const answered = props.answeredIds;
+    const unAnswered = questionIds.filter((q) => !answered.includes(q));
+
+    return (
+      <section className="py-5">
+        <div className="container mt-3">
+          <div className="row">
+            <QuestionsLayout answered={answered} unAnswered={unAnswered} />
+          </div>
+        </div>
+      </section>
+    );
+  }
+};
+
+const mapStateToProps = ({ users, authedUser, questions }) => {
+  const answered =
+    authedUser !== null ? Object.keys(users[authedUser].answers) : [];
+  return {
+    loaded: true,
+    authedUser,
+    authedUserDetails: users[authedUser],
+    answeredIds: answered.sort(
+      (a, b) => questions[b].timestamp - questions[a].timestamp
+    ),
+    questionIds: Object.keys(questions).sort(
+      (a, b) => questions[b].timestamp - questions[a].timestamp
+    )
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
