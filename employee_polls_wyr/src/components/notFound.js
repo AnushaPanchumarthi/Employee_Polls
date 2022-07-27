96f42491d113cb918d13f400892789a3dfc06dba
AnushaPanchumarthi <anooshap.rao@gmail.com>
2022-07-27 12:44:28 +0530

    Create notFound.js
    Initial commit

---
+++ employee_polls_wyr/src/components/notFound.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function notFound() {
+  return (
+    <section className="py-5 h-100">
+      <div className="container h-100 d-flex flex-column align-items-center justify-content-center">
+        <h1
+          className="display-1 mt-5"
+          style={{ fontSize: 204, fontWeight: 'bolder', color: '#D7DBDF' }}
+        >
+          404
+        </h1>
+        <h1 className="mt-2 font-weight-bold">Page not found!</h1>
+        <p className="lead mt-2">Unable to find the Page.</p>
+        <Link to="/home" className="mt-4 btn btn-dark px-4 py-2">
+          Back to Home
+        </Link>
+      </div>
+    </section>
+  );
+}
+
+export default notFound;
